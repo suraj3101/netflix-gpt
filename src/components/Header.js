f250a90ca5be16f8654ca0334a32052d73c18c4a
Suraj Gupta <surajgupta3101@gmail.com>
2024-09-16 16:48:22 +0530

    Redux && Auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import netflixLogo from "../assets/Netflix_Logo_PMS.png"
+import { auth } from '../utils/firebase'
+import { signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+
+  const user = useSelector(store => store.user);
+  console.log("Our user is: ", user);
+
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      navigate("/");
+    }).catch((error) => {
+      navigate("/error");
+    });
+  }
+
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black  z-10'>
+    <div className='absolute px-8 py-2 w-screen bg-gradient-to-b from-black justify-between z-10 flex'>
         <img
             className='w-44'
             src={netflixLogo}
             alt='netflix-logo'
         />
+        {user&&<div className='flex p-2'>
+          <img
+            src={user?.photoURL}
+            className='w-10 h-10'
+            alt='user-icon'
+          />
+          <button 
+            onClick={handleSignOut}
+            className='font-bold text-white'>Sign Out</button>
+        </div>}
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,23 +2,82 @@ import React, { useRef, useState } from "react";
 import Header from "./Header";
 import headerLogo from "../assets/header-logo.jpg";
 import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setisSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
 
   const handleButtonClick = () => {
     // Validate the form data:
-
-    console.log("Email: ", email.current.value);
-    console.log("Password: ", password.current.value); 
     const message = checkValidData(email.current.value, password.current.value);
     setErrorMessage(message);
-  }
+
+    if (message) return;
+
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/77961585?v=4",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
+  };
 
   const toggleSignInForm = () => {
     setisSignInForm(!isSignInForm);
@@ -26,18 +85,16 @@ const Login = () => {
 
   return (
     <div>
-
       <Header />
-      
+
       <div className="absolute">
         <img src={headerLogo} alt="headerLogo" />
       </div>
-      
-      <form 
+
+      <form
         onSubmit={(e) => e.preventDefault()}
         className="absolute p-8 bg-black my-36 mx-auto right-[32%] left-[32%] text-white rounded-lg bg-opacity-80"
       >
-        
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -67,13 +124,16 @@ const Login = () => {
 
         <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
 
-        <button className="p-2 my-3 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>
+        <button
+          className="p-2 my-3 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4 cursor-pointer" 
-            onClick={toggleSignInForm}
-        >
-          {isSignInForm ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now."}
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
         </p>
       </form>
     </div>
